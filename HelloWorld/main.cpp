ee96bbac743d6e4887815164326dc78c66e7bc82
Toan Vo <toanalien@gmail.com>
2016-10-22 22:43:13 +0700

    resize window

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QLabel *label = new QLabel("Hello World!");
     label->setWindowTitle("My First Qt App");
+    label->resize(400, 400);
     label->show();
 
     return app.exec();
