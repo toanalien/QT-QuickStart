94698e3ee0d9826238a6b32afb40070b36561eaf
Toan Vo <toanalien@gmail.com>
2016-10-22 22:41:16 +0700

    add label helloworld

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,10 @@
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    QLabel *label = new QLabel("Hello World!");
+    label->show();
 
     return app.exec();
 }
