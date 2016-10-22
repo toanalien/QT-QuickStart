06ae823d12dd08e1b5cbc36051534efb243bf492
Toan Vo <toanalien@gmail.com>
2016-10-22 22:42:27 +0700

    set windowtitle

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QLabel *label = new QLabel("Hello World!");
+    label->setWindowTitle("My First Qt App");
     label->show();
 
     return app.exec();
