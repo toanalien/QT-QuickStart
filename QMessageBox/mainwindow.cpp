cfb2bc52b2cdd779a528a2ddc6db0f0ba42d38ca
Toan Vo <toanalien@gmail.com>
2016-11-05 22:06:37 +0700

    MessageBox

--- QMessageBox/mainwindow.cpp
+++ QMessageBox/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qmessagebox.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +16,5 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-
+    QMessageBox::about(this, "My Title", "This is a custom message.");
 }
